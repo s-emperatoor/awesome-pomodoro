42921c399bbe2cea2f521b7b4277ca53ad7ae74d
s-emperatoor <sajjadkarbasi@yahoo.com>
2020-09-17 23:59:25 +0430

    fix shape issue and add arc chart insted of png and fix some minor bug

--- pomodoro.lua
+++ pomodoro.lua
@@ -7,16 +7,17 @@ local awful = require("awful")
 local path = awful.util.getdir("config") .."/pomodoro/"
 local pomodoro_image_path = awful.util.getdir("config") ..
                                 "/pomodoro/pomodoro.png"
+-- bonus timer
+local bonus
 
 -- values set by user
-local pomo_work_time = 20
-local pomo_break_time = 5
-local pomo_long_break_time = 10
+local pomo_work_time = 2*60
+local pomo_break_time = 1*60
+local pomo_long_break_time = 1.5*60
 local pomo_long_break_occur = 3
 
 -- the time that we use for caounting
-local time = 0
-
+local time_sec = 0
 
 -- timeoutes
 local work_timeout
@@ -26,6 +27,7 @@ local long_break_timeout
 -- intervals
 local started = false
 local activated = false
+local bonused = false
 local pomo_interval_work = 0
 local pomo_interval_break = 0
 local pomo_interval_long_break = 0
@@ -38,8 +40,10 @@ local pomo_state = 0
 
 -- widgets ======================================
 -- text widget that show the time counter
-local time_txt = wibox.widget {font = "play 9", widget = wibox.widget.textbox}
-
+local time_txt = wibox.widget {font = "play 7",align  = "center", widget = wibox.widget.textbox}
+local text_with_background = wibox.container.background(time_txt)
+text_with_background.bg = "#f75c7b"
+text_with_background.fg = '#000000'
 -- image widget that show the pomodoro icon
 local pomodoro = wibox.widget {
     image = pomodoro_image_path,
@@ -49,9 +53,23 @@ local pomodoro = wibox.widget {
 -- combiner of two widget in a flex layaut
 local pomodoro_shape = wibox.widget {
     pomodoro,
-    time_txt,
+    text_with_background,
     forced_width = 40,
     layout = wibox.layout.flex.horizontal
+}
+
+local pomodoro_widget = wibox.widget{
+    text_with_background,
+    bg = "#ffffff",
+    colors = {"#025c12"},
+    thickness = 3,
+    start_angle = 3*math.pi/2,
+    forced_height = 40,
+    forced_width = 40,
+    min_value = 0,
+    max_value = pomo_work_time,
+    value = time_sec,
+    widget = wibox.container.arcchart
 }
 -- END widgets ####################################
 
@@ -89,37 +107,61 @@ local long_break_timer = gears.timer({
 -- timeout functions ============================
 -- work timout
 work_timeout = function()
-    gears.debug.print_warning("run")
-    gears.debug.print_error("run")
-    time = 0
+    gears.debug.print_warning("work run")
+    time_sec = 0
     pomo_interval_work = pomo_interval_work + 1
     if (pomo_interval_work % pomo_long_break_occur == 0) then
-        long_break_timer:start()
+        if not long_break_timer.started then
+            long_break_timer:start()
+        else
+            long_break_timer:again()
+        end
         pomo_state = 2
-        pomodoro:set_image(path.."flower.png")
+        pomodoro_widget:set_colors({"#ed05af"})
+        pomodoro_widget:set_max_value(pomo_long_break_time)
     else
-        break_timer:start()
+        if not break_timer.started then
+            break_timer:start()
+        else
+            break_timer:again()
+        end
         pomo_state = 1
-        pomodoro:set_image(path.."clap.png")
+        pomodoro_widget:set_colors({"#e09e04"})
+        pomodoro_widget:set_max_value(pomo_break_time)
     end
+    bonused = false
 end
 
 -- break timeout
 break_timeout = function()
-    time = 0
+    gears.debug.print_warning("break run")
+    time_sec = 0
     pomo_state = 0
     pomo_interval_break = pomo_interval_break + 1
-    work_timer:start()
-    pomodoro:set_image(path.."pomodoro.png")
+    if not work_timer.started then
+        work_timer:start()
+    else
+        work_timer:again()
+    end
+    pomodoro_widget:set_colors({"#025c12"})
+    pomodoro_widget:set_max_value(pomo_work_time)
+    bonused = false
 end
 
 -- long break timeout
 long_break_timeout = function()
-    time = 0
+    gears.debug.print_warning("long break run")
+    time_sec = 0
     pomo_state = 0
     pomo_interval_long_break = pomo_interval_long_break + 1
-    work_timer:start()
-    pomodoro:set_image(path.."pomodoro.png")
+    if not work_timer.started then
+        work_timer:start()
+    else
+        work_timer:again()
+    end
+    pomodoro_widget:set_colors({"#025c12"})
+    pomodoro_widget:set_max_value(pomo_work_time)
+    bonused = false
 end
 -- END timeout functions #########################
 
@@ -127,75 +169,89 @@ end
 -- working callback
 work_timer:connect_signal("timeout",work_timeout)
 
--- short break callback 
+-- short break callback
 break_timer:connect_signal("timeout", break_timeout)
 
 -- long break callback
 long_break_timer:connect_signal("timeout", long_break_timeout)
 
+-- interavl cllback
 interval:connect_signal("timeout", function()
-    time = time + 1
-    time_txt:set_markup(time)
+    time_sec = time_sec + 1
+    time_txt:set_markup("<span foreground='black' size='small'>"..math.floor(time_sec/60).."</span>"..":".."<span foreground='blue' size='xx-small'>"..(time_sec%60).."</span>")
+    pomodoro_widget.value = (time_sec)
     -- gears.debug.print_warning(time)
 end)
 -- END callback ##################################
 
-pomodoro_shape:buttons(awful.util.table.join(awful.button({}, 1,
+-- pomodoro_shape:buttons(awful.util.table.join(awful.button({}, 1,
+pomodoro_widget:buttons(awful.util.table.join(awful.button({}, 1,
     function()
         if not started then
             interval:start()
             work_timer:start()
             started = true
             activated = true
+            text_with_background.bg = "#91ff96"
         else
-            if activated then                
-                if pomo_state == 0 then
-                    interval:stop()
-                    work_timer:stop()
-                elseif pomo_state == 1 then
-                    interval:stop()
-                    break_timer:stop()
-                elseif pomo_state == 2 then
-                    interval:stop()
-                    long_break_timer:stop()
+            if activated then
+                if not bonused then
+                    if pomo_state == 0 then
+                        work_timer:stop()
+                    elseif pomo_state == 1 then
+                        break_timer:stop()
+                    elseif pomo_state == 2 then
+                        long_break_timer:stop()
+                    end
+                else
+                    bonus:stop()
+                    bonused = false
                 end
+                interval:stop()
+                text_with_background.bg = "#f75c7b"
                 activated = false
             else
-                local bonus
                 if pomo_state == 0 then
+                    local t = pomo_work_time - time_sec
                     bonus = gears.timer({
-                        timeout = pomo_work_time - time,
+                        timeout = t,
                         call_now = false,
                         autostart = false,
                         single_shot = true,
-                        callback = long_break_timeout
                     })
-                    interval:start()
-                elseif pomo_state == 1 then
-                    bonus = gears.timer({
-                        timeout = pomo_break_time - time,
-                        call_now = false,
-                        autostart = false,
-                        single_shot = true,
-                        callback = break_timeout
-                    })
-                    interval:start()
-                elseif pomo_state == 2 then
-                    bonus = gears.timer({
-                        timeout = pomo_long_break_time - time,
-                        call_now = false,
-                        autostart = false,
-                        single_shot = true,
-                        callback = long_break_timeout
-                    })
+                    bonus:connect_signal("timeout",work_timeout)
                     bonus:start()
-                    interval:start()
+                    gears.debug.print_warning("state = 0 bonus run with time : "..t)
+                elseif pomo_state == 1 then
+                    local t = pomo_break_time - time_sec
+                    bonus = gears.timer({
+                        timeout = t,
+                        call_now = false,
+                        autostart = false,
+                        single_shot = true,
+                    })
+                    bonus:connect_signal("timeout",work_timeout)
+                    bonus:start()
+                    gears.debug.print_warning("state = 1 bonus run with time : "..t)
+                elseif pomo_state == 2 then
+                    local t = pomo_long_break_time - time_sec
+                    bonus = gears.timer({
+                        timeout = t,
+                        call_now = false,
+                        autostart = false,
+                        single_shot = true,
+                    })
+                    bonus:connect_signal("timeout",work_timeout)
+                    bonus:start()
+                    gears.debug.print_warning("state = 2 bonus run with time : "..t)
                 end
-                bonus:start()
+                interval:start()
+                text_with_background.bg = "#91ff96"
                 activated = true
+                bonused = true
             end
-            
+
         end
     end)))
 
-return pomodoro_shape
+return pomodoro_widget
